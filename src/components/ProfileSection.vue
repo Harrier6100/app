42052587eb832efcb67e53cf8bd0394a98dec129
Harrier6100 <harrier6100@gmail.com>
2025-05-27 08:00:50 +0900

    [1.01]

---
+++ src/components/ProfileSection.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="border-top py-3" role="button" @click="props.toggle">
+            <div class="d-flex">
+                <div class="me-auto">
+                    <a class="text-dark text-decoration-none">{{ props.label }}</a>
+                </div>
+                <i :class="props.isOpen ? 'bi bi-chevron-down' : 'bi bi-chevron-right'"></i>
+            </div>
+        </div>
+        <div v-if="props.isOpen">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    label: String,
+    isOpen: Boolean,
+    toggle: Function,
+});
+</script>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,35 +1,27 @@
 <template>
     <div class="container">
         <h5 class="mb-3">設定</h5>
-        <div class="border-top border-bottom py-3" role="button" @click="menu1.toggle">
-            <div class="d-flex">
-                <div class="me-auto">
-                    <a class="text-dark text-decoration-none">名前・メールアドレス</a>
-                </div>
-                <i class="bi bi-chevron-right" v-if="!menu1.isOpen.value"></i>
-                <i class="bi bi-chevron-down" v-if="menu1.isOpen.value"></i>
-            </div>
-            <div class v-if="menu1.isOpen.value">
-                aaa
-            </div>
-        </div>
-        <div class="border-bottom py-3" role="button" @click="menu2.toggle">
-            <div class="d-flex">
-                <div class="me-auto">
-                    <a class="text-dark text-decoration-none">パスワード</a>
-                </div>
-                <i class="bi bi-chevron-right" v-if="!menu2.isOpen.value"></i>
-                <i class="bi bi-chevron-down" v-if="menu2.isOpen.value"></i>
-            </div>
-            <div class v-if="menu2.isOpen.value">
-                aaa
-            </div>
-        </div>
+        <ProfileSection
+            label="名前・メールアドレス"
+            :isOpen="menu1.isOpen.value"
+            :toggle="menu1.toggle"
+        >
+            <div>aaa</div>
+        </ProfileSection>
+        <ProfileSection
+            label="パスワード"
+            :isOpen="menu2.isOpen.value"
+            :toggle="menu2.toggle"
+        >
+            <div>bbb</div>
+        </ProfileSection>
+        <div class="border-bottom"></div>
     </div>
 </template>
 
 <script setup>
 import { useOpenClose } from '@/composables/useOpenClose';
+import ProfileSection from '@/components/ProfileSection';
 
 const menu1 = useOpenClose();
 const menu2 = useOpenClose();
