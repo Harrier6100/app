37585ac97151cc9d07908f4658de7ea9ae64c22d
Harrier6100 <harrier6100@gmail.com>
2025-06-12 09:13:56 +0900

    [1.03]

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,14 +1,22 @@
 <template>
     <h6 class="mb-3">設定</h6>
 
-    <div class="row">
-        <div class="col-4">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">名前</li>
-                <li class="list-group-item">パスワード</li>
-            </ul>
+    <div class="d-flex flex-column flex-md-row mb-3 align-items-center">
+        <div class="flex-shrink-0 w-100 p-2 fw-bold">名前</div>
+        <div class="flex-grow-1 bg-light p-2 rounded">ue migi</div>
+    </div>
+    <div class="border-top">
+        <div class="d-flex flex-column flex-md-row mb-3">
+            <div class="flex-shrink-0 w-100 p-2">名前</div>
+            <div class="flex-grow-1 bg-light p-2">ue migi
+            </div>
         </div>
-        <div class="col-8">
+    </div>
+    <div class="border-top border-bottom">
+        <div class="d-flex flex-column flex-md-row mb-3">
+            <div class="flex-shrink-0 w-100 w-25 p-2">メールアドレス</div>
+            <div class="flex-grow-1 bg-light p-2">shita migi
+            </div>
         </div>
     </div>
 </template>
